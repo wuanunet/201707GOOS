8d1d54f7bf21dd3a3a09e5fbc784c5cc76bf416e
Cheng-Ju Wu <chengjuwu@nine-yi.com>
2017-07-01 11:24:57 +0800

    add a failed accetpance testing.

---
+++ GOOS_SampleTests/PageObjects/BudgetCreatePage.cs
@@ -0,0 +1,31 @@
+﻿using FluentAutomation;
+
+namespace GOOS_SampleTests.PageObjects
+{
+    internal class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+        }
+
+        public BudgetCreatePage Amount(int amount)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public BudgetCreatePage Month(string yearMonth)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddBudget()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShouldDisplay(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ GOOS_SampleTests/Steps/BudgetCreateSteps.cs
@@ -0,0 +1,46 @@
+﻿using FluentAutomation;
+using GOOS_SampleTests.PageObjects;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.Steps
+{
+    [Binding]
+    [Scope(Feature = "BudgetCreate")]
+    public class BudgetCreateSteps : FluentTest
+    {
+        private BudgetCreatePage _budgetCreatePage;
+
+        public BudgetCreateSteps()
+        {
+            this._budgetCreatePage = new BudgetCreatePage(this);
+        }
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+
+        [Given(@"go to adding budget page")]
+        public void GivenGoToAddingBudgetPage()
+        {
+            this._budgetCreatePage.Go();
+        }
+
+        [When(@"I add a buget (.*) for ""(.*)""")]
+        public void WhenIAddABugetFor(int amount, string yearMonth)
+        {
+
+            this._budgetCreatePage
+                .Amount(amount)
+                .Month(yearMonth)
+                .AddBudget();
+        }
+
+        [Then(@"it should display ""(.*)""")]
+        public void ThenItShouldDisplay(string message)
+        {
+            this._budgetCreatePage.ShouldDisplay(message);
+        }
+    }
+}
